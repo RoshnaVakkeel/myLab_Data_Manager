7b7be4d9d38fa13225cb423740ce013119396aeb
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2022-11-13 22:29:26 +0000

    Wire up APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('mylab_data')
+
+chem_inventory = SHEET.worksheet('chem_inventory')
+data = chem_inventory.get_all_values()
+print (data)
